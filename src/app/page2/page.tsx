4bd4013b42d266f3f5dac1758729309a224a55f3
Minsu Kim <67222970+kms0219kms@users.noreply.github.com>
2024-11-28 01:27:56 +0700

    :lipstick: design: 폰트 추가, 글자 vw로 처리하면서 글자가 지나치게 작아지는 문제 임시 수정

--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -1,12 +1,16 @@
+import { nanumMyeongjo, playfairDisplay } from "@/styles/fonts";
 import styles from "./styles.module.css";
 
 export default function Page2() {
   return (
     <div className={styles.container}>
-      <div className={styles.subcontainer}>
-        <div className={styles.title}>The Final</div>
-        <div className={styles.subtitle}>The Last Song of Stellar</div>
-        <div className={styles.description}>아이리 칸나의 마지막 콘서트</div>
+      <div className={`${styles.title} ${playfairDisplay.className}`}>
+        The Finale
+      </div>
+
+      <div className={styles.subtitle}>The Last Song of Stellar</div>
+      <div className={`${styles.description} ${nanumMyeongjo.className}`}>
+        아이리 칸나의 마지막 콘서트
       </div>
     </div>
   );

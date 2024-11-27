0c6a325ff32bebfe65831269193177dff37dcfe9
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-27 11:28:12 -0500

    update page (TESTED)
    update page (TESTED)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import Page1 from "./page1/page";
+import Page1_1 from "./page1_1/page";
 import Page2 from "./page2/page";
 
 export default function HomePage() {
   const sections = [
     { id: 1, component: <Page1 /> },
+    { id: 1_1, component: <Page1_1 /> },
     { id: 2, component: <Page2 /> },
     { id: 3, component: <div>Page 3</div> },
   ];

29ae5b4eba0544118e30c4a6279cd52aec695d3d
lesuyo0621 <lesuyo930621@naver.com>
2024-11-27 19:53:56 +0900

    feat: 두 번째 섹션 The Finale 추가

--- pages/index.vue
+++ pages/index.vue
@@ -8,13 +8,20 @@
             <p class="wallpaper-sub">마지막 별의 노래가 다가오는 날</p>
         </div>
     </section>
+    <section class="concert-title-section">
+        <p class="title">The Finale</p>
+        <p class="title-sub">The last song of Stellar</p>
+        <p class="info">아이리 칸나의 마지막 콘서트</p>
+    </section>
 </template>
 
 <style scoped>
+    /* 최대 너비를 1920px로 설정 */
     section {
         max-width: 1920px;
     }
 
+    /* 첫 번째 섹션 - 디데이 카운트 있는 배너 */
     .wallpaper-with-time-section {
         width: 100%;
         height: 878px;
@@ -74,12 +81,49 @@
         letter-spacing: -0.54px;
         font-weigth: 600;
     }
+
+    /* 두 번째 섹션 - The Finale */
+    .concert-title-section {
+        width: 100%;
+        height: 249px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: black;
+    }
+    
+    .concert-title-section > .title {
+        color: white;
+        font-family: Playfair Display;
+        font-size: min(3.43vw, 66px);
+        font-weight: 700;
+        letter-spacing: 0.33px;
+        margin-bottom: 5px;
+    }
+
+    .concert-title-section > .title-sub {
+        color: #777;
+        font-family: Georgia;
+        font-size: min(0.83vw, 16px);
+        font-weight: 400;
+        letter-spacing: 6.72px;
+        margin-bottom: 25px;
+    }
+
+    .concert-title-section > .info {
+        color: #CAC9D9;
+        font-family: Nanum Myeongjo;
+        font-size: min(1.35vw, 26px);
+        font-weight: 700;
+    }
 </style>
 
 <style>
     @import url("https://cdn.jsdelivr.net/gh/wanteddev/wanted-sans@v1.0.3/packages/wanted-sans/fonts/webfonts/variable/split/WantedSansVariable.min.css");
-    @import url('//fonts.googleapis.com/earlyaccess/nanummyeongjo.css');
+    @import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap');
     @import url('https://cdn.jsdelivr.net/npm/pretendard/dist/pretendard.css');
+    @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap');
 
     body,
     p {

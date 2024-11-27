42569d4cc46cf81c562e26319667142abac2af48
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-27 12:48:28 -0500

    mv page 3 to 1_1
    mv page 3 to 1_1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Page1 from "./page1/page";
 import Page1_1 from "./page1_1/page";
 import Page1_2 from "./page1_2/page";
 import Page2 from "./page2/page";
-import Page3 from "./page3/page";
 
 export default function HomePage() {
   const sections = [
@@ -12,7 +11,6 @@ export default function HomePage() {
     { id: 1_1, component: <Page1_1 /> },
     { id: 1_2, component: <Page1_2 /> },
     { id: 2, component: <Page2 /> },
-    { id: 3, component: <Page3 /> },
   ];
 
   return (
--- src/app/page1_1/page.tsx
+++ src/app/page1_1/page.tsx
@@ -1,33 +1,35 @@
-"use client";
+'use client';
+import { useEffect, useRef } from 'react';
+import styles from './styles.module.css';
 
-import { useEffect, useRef } from "react";
+export default function Page2() {
+    const videoRef = useRef<HTMLVideoElement>(null);
 
-import styles from "./styles.module.css";
+    useEffect(() => {
+      if (videoRef.current) {
+        videoRef.current.play().catch((err) => {
+          console.error("Video playback failed:", err);
+        });
+      }
+    }, []);
 
-function Page1_1() {
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.play().catch((err) => {
-        console.error("Video playback failed:", err);
-      });
-    }
-  }, []);
-
-  return (
-    <div>
-      <video
-        ref={videoRef}
-        className={styles.video}
-        src="https://www.mdoo.info/airi_kanna/wallpaper.mp4"
-        muted
-        loop
-        playsInline
-        autoPlay
-      />
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <video
+                ref={videoRef}
+                className={styles.video}
+                src="https://www.mdoo.info/airi_kanna/wallpaper.mp4"
+                muted
+                loop
+                playsInline
+                autoPlay
+            />
+            <img className={styles.logo} src="The_finale_logo3.png"/>
+            <div className={styles.left__bar}></div>
+            <div className={styles.right__bar}></div>
+            <div className={styles.bottom__bar}></div>
+            <p className={`${styles.left__content} ${styles.raleway__medium}`}>The last song</p>
+            <p className={`${styles.right__content} ${styles.raleway__medium}`}>of Stellar</p>
+        </div>
+    );
 }
-
-export default Page1_1;

2f9c2afeb5d2a6a7d57710df9b413d80c4ec032d
lesuyo0621 <lesuyo930621@naver.com>
2024-11-27 10:26:07 +0900

    design: index.vue / 전역으로 필요한 css 설정 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,13 @@
-<template></template>
+<template>
+    <section>
+
+    </section>
+</template>
+
+<style>
+    @import url("https://cdn.jsdelivr.net/gh/wanteddev/wanted-sans@v1.0.3/packages/wanted-sans/fonts/webfonts/variable/split/WantedSansVariable.min.css");
+
+    body {
+        margin: 0;
+    }
+</style>

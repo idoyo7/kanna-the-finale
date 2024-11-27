80e310f90416b2cf33e74e5d59d2cb637d186802
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-27 09:38:59 -0500

    Update getTimer (TESTED)
    Update getTimer (TESTED)

---
+++ src/app/api/getTimer/route.ts
@@ -0,0 +1,7 @@
+// api/getTimer/route.ts
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+    return NextResponse.json({ message: 'Hello, World!' });
+}

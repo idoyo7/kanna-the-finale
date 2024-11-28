facfe4a0a4d372d8f8ef2843e727ee4005c3a449
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-28 00:16:09 -0500

    1차 마감

--- src/app/page9/page.tsx
+++ src/app/page9/page.tsx
@@ -6,12 +6,12 @@ function Page9() {
             <div className={styles.title}>선물함</div>
             <div className={styles.giftboxContainer}>
                 <div className={styles.giftbox}>
-                    <img src="/gift.png" alt="gift" style={{width: '22.5vh', height: '22.5vh', margin: '2.5%'}} />
-                    <div>[기간한정] 아이리 칸나 모바일, 탭 배경화면 공유 </div>
+                    <img src="/gift1.jpg" alt="gift" style={{width: '22.5vh', height: '22.5vh', margin: '2.5%'}} />
+                    <div className={styles.giftboxTitle}>[기간한정] 아이리 칸나 모바일, 탭 배경화면 공유 </div>
                 </div>
                 <div className={styles.giftbox}>
                     <img src="/gift2.jpg" alt="gift" style={{width: '22.5vh', height: '22.5vh', margin: '2.5%'}} />
-                    <div>[기간한정] 아이리 칸나 월페이퍼 공유 </div>
+                    <div className={styles.giftboxTitle}>[기간한정] 아이리 칸나 월페이퍼 공유 </div>
                 </div>
             </div>
         </div>

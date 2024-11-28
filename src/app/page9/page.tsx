38e7f99bf776dd8275db7ac7f3a095ced6f9840b
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-27 23:52:17 -0500

    Update page.tsx

--- src/app/page9/page.tsx
+++ src/app/page9/page.tsx
@@ -1,1 +1,11 @@
-// 선물함
+import styles from './styles.module.css';
+
+function Page9() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.title}>◦ 아이리 칸나의 선물함 ◦</div>
+        </div>
+    );
+}
+
+export default Page9;

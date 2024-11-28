6fbb85e0e00cf81c546d91546ff69991908bb2f5
kms0219kms <kms0219kms@naver.com>
2024-11-28 23:01:23 +0700

    :adhesive_bandage: fix: 각 섹션에 직접 접속 불가능하도록 경로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import styles from "@/modules/styles/root.module.css";
 import Section from "@/modules/section/section";
 
-import Page1 from "./page1/page";
-import Page2 from "./page2/page";
-import Page3 from "./page3/page";
-import Page4 from "./page4/page";
-import Page5 from "./page5/page";
-import Page6 from "./page6/page";
-import Page7 from "./page7/page";
-import Page8 from "./page8/page";
-import Page9 from "./page9/page";
-import Page10 from "./page10/page";
+import Page1 from "@/sections/page1/page";
+import Page2 from "@/sections/page2/page";
+import Page3 from "@/sections/page3/page";
+import Page4 from "@/sections/page4/page";
+import Page5 from "@/sections/page5/page";
+import Page6 from "@/sections/page6/page";
+import Page7 from "@/sections/page7/page";
+import Page8 from "@/sections/page8/page";
+import Page9 from "@/sections/page9/page";
+import Page10 from "@/sections/page10/page";
 
 export default function HomePage() {
   const sections = [

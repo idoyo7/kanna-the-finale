3377de5b87bca436a8d5b33d4906c44cd9c6c8cb
kms0219kms <kms0219kms@naver.com>
2024-12-02 16:58:46 +0700

    :alien: fix: 콘서트 시간 20시로 변경됨에 따른 반영

--- src/modules/time/calcTime.ts
+++ src/modules/time/calcTime.ts
@@ -1,6 +1,10 @@
 // 하드코딩된 목표 날짜 (2024-12-02 19:00:00 KST)
-export const TARGET_DATE = new Date("2024-12-02T19:00:00+09:00");
-export const DOWNLOAD_DEADLINE = new Date("2024-12-02T23:59:59+09:00");
+// export const TARGET_DATE = new Date("2024-12-02T19:00:00+09:00");
+// 20241202 Minsu Kim _ 콘서트 시간 20시로 변경되어 반영
+export const TARGET_DATE = new Date("2024-12-02T20:00:00+09:00");
+
+// 하드코딩된 다운로드 기한 (2024-12-03 00:00:00 KST)
+export const DOWNLOAD_DEADLINE = new Date("2024-12-03T00:00:00+09:00");
 
 export const concertPlaying = () => {
   const now = new Date();

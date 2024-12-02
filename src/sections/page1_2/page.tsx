30036cfc497b859bc329357bc096dfc40111b5b6
Minsu Kim <minsu_kim@bishanoi.net>
2024-12-02 08:03:12 +0000

    :sparkles: feat: 마지막 커밋_팜플렛 버튼 추가

--- src/sections/page1_2/page.tsx
+++ src/sections/page1_2/page.tsx
@@ -46,59 +46,88 @@ export default function Page1_2() {
           <div className={styles.right__bar}></div>
         </div>
       </div>
-      <div className={styles.ctas_group}>
-        <button
-          className={styles.cta}
-          onClick={() => {
-            const pvSection = document.getElementById("pv");
-            if (pvSection) {
-              pvSection.scrollIntoView({ behavior: "smooth" });
-            }
-          }}
-        >
-          PV 보러가기{" "}
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M8.75147 17.6485C8.28284 17.1799 8.28284 16.4201 8.75147 15.9515L12.7029 12L8.75147 8.04853C8.28284 7.5799 8.28284 6.8201 8.75147 6.35147C9.22009 5.88284 9.97989 5.88284 10.4485 6.35147L15.2485 11.1515C15.7172 11.6201 15.7172 12.3799 15.2485 12.8485L10.4485 17.6485C9.97989 18.1172 9.2201 18.1172 8.75147 17.6485Z"
-              ></path>
-            </svg>
-          </span>
-        </button>
 
-        <button
-          className={styles.cta}
-          onClick={() => {
-            window.open(
-              "https://chzzk.naver.com/live/f722959d1b8e651bd56209b343932c01"
-            );
-          }}
-        >
-          콘서트 보러가기{" "}
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M8.75147 17.6485C8.28284 17.1799 8.28284 16.4201 8.75147 15.9515L12.7029 12L8.75147 8.04853C8.28284 7.5799 8.28284 6.8201 8.75147 6.35147C9.22009 5.88284 9.97989 5.88284 10.4485 6.35147L15.2485 11.1515C15.7172 11.6201 15.7172 12.3799 15.2485 12.8485L10.4485 17.6485C9.97989 18.1172 9.2201 18.1172 8.75147 17.6485Z"
-              ></path>
-            </svg>
-          </span>
-        </button>
+      <div className={styles.ctas_group}>
+        <div className={styles.ctas_subgroup}>
+          <button
+            className={styles.cta}
+            onClick={() => {
+              const pvSection = document.getElementById("pv");
+              if (pvSection) {
+                pvSection.scrollIntoView({ behavior: "smooth" });
+              }
+            }}
+          >
+            PV 보러가기{" "}
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M8.75147 17.6485C8.28284 17.1799 8.28284 16.4201 8.75147 15.9515L12.7029 12L8.75147 8.04853C8.28284 7.5799 8.28284 6.8201 8.75147 6.35147C9.22009 5.88284 9.97989 5.88284 10.4485 6.35147L15.2485 11.1515C15.7172 11.6201 15.7172 12.3799 15.2485 12.8485L10.4485 17.6485C9.97989 18.1172 9.2201 18.1172 8.75147 17.6485Z"
+                ></path>
+              </svg>
+            </span>
+          </button>
+        </div>
+
+        <div className={styles.ctas_subgroup}>
+          <button
+            className={styles.cta}
+            onClick={() => {
+              window.open(formatUrl("/gifts/pamphlet.pdf"));
+            }}
+          >
+            셋리스트 다운로드{" "}
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M8.75147 17.6485C8.28284 17.1799 8.28284 16.4201 8.75147 15.9515L12.7029 12L8.75147 8.04853C8.28284 7.5799 8.28284 6.8201 8.75147 6.35147C9.22009 5.88284 9.97989 5.88284 10.4485 6.35147L15.2485 11.1515C15.7172 11.6201 15.7172 12.3799 15.2485 12.8485L10.4485 17.6485C9.97989 18.1172 9.2201 18.1172 8.75147 17.6485Z"
+                ></path>
+              </svg>
+            </span>
+          </button>
+
+          <button
+            className={styles.cta}
+            onClick={() => {
+              window.open(
+                "https://chzzk.naver.com/live/f722959d1b8e651bd56209b343932c01"
+              );
+            }}
+          >
+            콘서트 보러가기{" "}
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M8.75147 17.6485C8.28284 17.1799 8.28284 16.4201 8.75147 15.9515L12.7029 12L8.75147 8.04853C8.28284 7.5799 8.28284 6.8201 8.75147 6.35147C9.22009 5.88284 9.97989 5.88284 10.4485 6.35147L15.2485 11.1515C15.7172 11.6201 15.7172 12.3799 15.2485 12.8485L10.4485 17.6485C9.97989 18.1172 9.2201 18.1172 8.75147 17.6485Z"
+                ></path>
+              </svg>
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );

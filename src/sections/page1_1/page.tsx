98f80a18d5b3bcbfc40254714240581fd708f158
root <api.test9989@gmail.com>
2024-12-05 15:49:34 +0000

    fix clock

--- src/sections/page1_1/page.tsx
+++ src/sections/page1_1/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef, useState } from "react";
 
 import formatUrl from "@/modules/cdn/formatUrl";
-import { TARGET_DATE } from "@/modules/time/calcTime";
+import { TARGET_DATE, concertPlaying } from "@/modules/time/calcTime";
 
 import { notoSansKr } from "@/modules/styles/fonts";
 import styles from "./styles.module.css";
@@ -17,32 +17,33 @@ export default function Page1_1() {
     hours: 0,
     minutes: 0,
     seconds: 0,
-    isPast: false, // 공연이 지났는지 여부
   });
+
+  // 공연이 종료되었는지 확인
+  const [isPast, setIsPast] = useState(concertPlaying());
 
   // 클라이언트에서 남은 시간 계산
   useEffect(() => {
     const calculateTimeLeft = () => {
       const now = new Date();
-      const timeDiff = now.getTime() - TARGET_DATE.getTime(); // 현재 시간이 TARGET_DATE 이후인지 확인
+      const timeDiff = Math.abs(now.getTime() - TARGET_DATE.getTime());
 
-      const isPast = timeDiff > 0; // 시간이 지난 경우
-      const absoluteDiff = Math.abs(timeDiff);
-
-      const days = Math.floor(absoluteDiff / (1000 * 60 * 60 * 24));
+      const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
       const hours = Math.floor(
-        (absoluteDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
       );
-      const minutes = Math.floor((absoluteDiff % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((absoluteDiff % (1000 * 60)) / 1000);
+      const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
       setTimer({
         days,
         hours,
         minutes,
         seconds,
-        isPast,
       });
+
+      // 공연이 종료되었는지 업데이트
+      setIsPast(concertPlaying());
     };
 
     calculateTimeLeft(); // 초기 계산
@@ -51,7 +52,7 @@ export default function Page1_1() {
     return () => clearInterval(interval); // 컴포넌트 언마운트 시 클린업
   }, []);
 
-  // 비디오 재생 로직 수정
+  // 비디오 재생 로직
   useEffect(() => {
     const playVideo = async () => {
       if (videoRef.current) {
@@ -84,7 +85,7 @@ export default function Page1_1() {
 
       <div className={styles.info__wrap}>
         <p className={styles.countdown__day}>
-          {timer.isPast ? `D+${timer.days}` : `D-0`}
+          {isPast ? `D+${timer.days}` : `D-${timer.days}`}
         </p>
         <p className={`${styles.countdown__time} ${notoSansKr.className}`}>
           {`${timer.hours.toString().padStart(2, "0")}:${timer.minutes
@@ -92,10 +93,10 @@ export default function Page1_1() {
             .padStart(2, "0")}:${timer.seconds.toString().padStart(2, "0")}`}
         </p>
         <p className={`${styles.title} ${notoSansKr.className}`}>
-          {timer.isPast ? "CONCERT ENDED" : "COMING SOON"}
+          {isPast ? "CONCERT ENDED" : "COMING SOON"}
         </p>
         <p className={styles.sub}>
-          {timer.isPast
+          {isPast
             ? "공연이 끝났습니다. 여운을 함께 느껴보세요!"
             : "마지막 별의 노래가 다가오는 날"}
         </p>

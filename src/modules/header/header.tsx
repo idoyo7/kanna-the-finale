76a13fb5f3e8c58d08708fd46795306538ec48b8
root <api.test9989@gmail.com>
2024-12-12 15:26:38 +0000

    fix banner selector

--- src/modules/header/header.tsx
+++ src/modules/header/header.tsx
@@ -86,7 +86,7 @@ function Header() {
     { id: "hero", alternatives: ["hero"], label: "메인" },
     { id: "pv", alternatives: ["banner", "pv", "story"], label: "Concert" },
     { id: "history", alternatives: ["history", "history2"], label: "보석함" },
-    { id: "hipbanner", alternatives: ["hipbanner"], label: "플레이리스트" },
+    { id: "hiphop", alternatives: ["hiphop"], label: "플레이리스트" },
     {
       id: "post",
       alternatives: ["post", "giftbox", "share"],
